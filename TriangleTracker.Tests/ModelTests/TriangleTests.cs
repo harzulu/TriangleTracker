4f67cdb0260bd1364e1cee9cdfa40b37dafa949e
Benjamin McFarland <benrmcfarland@gmail.com>
2020-12-08 13:46:55 -0800

    Add second fail test

--- TriangleTracker.Tests/ModelTests/TriangleTests.cs
+++ TriangleTracker.Tests/ModelTests/TriangleTests.cs
@@ -7,11 +7,19 @@ namespace TriangleTracker.Tests
   public class TriangleTests
   {
     [TestMethod]
-    public void ReturnNums_ReturnTheGivenNumbers_Num1Num2Num3()
+    public void ReturnNums_ReturnTheFirstGivenNumber_Num1()
     {
       int[] sides = {3, 4, 8};
       Triangle testTriangle = new Triangle();
       Assert.AreEqual(3, testTriangle.ReturnNums(sides));
+    }
+
+    [TestMethod]
+    public void AnyDuplicates_ReturnIfThereAreDuplicates_True()
+    {
+      int[] sides = {3, 4, 8};
+      Triangle testTriangle = new Triangle();
+      Assert.AreEqual(true, testTriangle.AnyDuplicates(sides));
     }
   }
 }
--- TriangleTracker/Models/Triangle.cs
+++ TriangleTracker/Models/Triangle.cs
@@ -6,5 +6,22 @@ namespace TriangleTracker
     {
       return arr[0];
     }
+
+    public bool AnyDuplicates(int[] arr)
+    {
+      if (arr[0] == arr[1] || arr[0] == arr[2])
+      {
+        return true;
+      } else 
+      {
+        if (arr[1] == arr[2])
+        {
+          return true;
+        } else
+        {
+          return false;
+        }
+      }
+    }
   }
 }
